9fff0946288fbdb6ef4f55dc83082b76c68c6dc9
raonifst <raonifst@gmail.com>
2014-10-07 17:52:30 -0300

    Incluindo os códigos da aula de 07 de outubro

---
+++ code/07_10/2.c
@@ -0,0 +1,39 @@
+/*
+DESCRIÇÃO: Este programa lê os dígitos da placa de um determinado
+           veículo e imprime o dia da semana em que este veículo
+           não pode circular de acordo com as regras do rodízio
+           em São Paulo.
+
+ENTRADA:   Um número inteiro representando os dígitos da placa.
+
+SAÍDA:     Uma cadeia de caracteres indicado o respectivo dia da
+           da semana (Segunda-feira, Terça-feira, Quarta-feira,
+           Quinta-feira, Sexta-feira)
+
+AUTOR: <nome aluno> RGA: <2014...>
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    /**/
+    int n, d;
+    scanf("%d", &n);
+
+    d = n % 10; /*Por que?*/
+
+    if(d == 1 || d == 2)
+        printf("Segunda-feira\n");
+    if(d == 3 || d == 4)
+        printf("Terca-feira\n");
+    if(d == 5 || d == 6)
+        printf("Quarta-feira\n");
+    if(d == 7 || d == 8)
+        printf("Quinta-feira\n");
+    if(d == 9 || d == 0)
+        printf("Sexta-feira\n");
+
+
+    return 0;
+}
---
+++ code/07_10/4.c
@@ -0,0 +1,51 @@
+/*
+DESCRIÇÃO: Este programa lê os dígitos da placa de um determinado
+           veículo e imprime o dia da semana em que este veículo
+           não pode circular de acordo com as regras do rodízio
+           em São Paulo.
+
+ENTRADA:   Um número inteiro representando os dígitos da placa.
+
+SAÍDA:     Uma cadeia de caracteres indicado o respectivo dia da
+           da semana (Segunda-feira, Terça-feira, Quarta-feira,
+           Quinta-feira, Sexta-feira)
+
+AUTOR: <nome aluno> RGA: <2014...>
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    /**/
+    int n, d;
+    scanf("%d", &n);
+
+    d = n % 10;
+
+    switch(d) {
+        case 1:
+        case 2:
+            printf("Segunda-feira\n");
+        break;
+        case 3:
+        case 4:
+            printf("Terça-feira\n");
+        break;
+        case 5:
+        case 6:
+            printf("Quarta-feira\n");
+        break;
+        case 7:
+        case 8:
+            printf("Quinta-feira\n");
+        break;
+        case 9:
+        case 0:
+            printf("Sexta-feira\n");
+        break;
+    }
+
+
+    return 0;
+}
